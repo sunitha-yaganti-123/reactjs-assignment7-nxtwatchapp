55a282b77b8db62280e1a65a73be95fe5faf3238
sunitha_yaganti <yvvlsunitha@gmail.com>
2022-08-04 10:20:15 +0000

    message

---
+++ src/context/NxtWatchContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NxtWatchContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  addToSaveVideos: () => {},
+  activeTabItem: () => {},
+  activeTab: '',
+  onChangeTheme: () => {},
+  removeSaveVideos: () => {},
+})
+export default NxtWatchContext
